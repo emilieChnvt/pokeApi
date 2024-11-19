5cdf1f81ce096d954df6d60b145f5399438d220b
Emilie <emilie.chnvt@gmail.com>
2024-11-19 13:07:22 +0100

    tooooooo long to appears

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ fetch(url)
             let cardPokemon = `
                 <div class="col-3">
                     <div class="card" style="width: 18rem;">
-                        <img src="${pokemon.image}" class="card-img-top" alt="...">
+                        <img src="${pokemon.image}" class="card-img-top w-100" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${pokemon.name}</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
