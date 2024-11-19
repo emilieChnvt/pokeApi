8686630c2f37e805d7952cd83f813f9babbb4973
Emilie <emilie.chnvt@gmail.com>
2024-11-19 13:01:15 +0100

    all pokemons added

--- js/main.js
+++ js/main.js
@@ -8,5 +8,18 @@ fetch(url)
  .then(response => response.json())
     .then(data => {
         data.forEach((pokemon) => {
-            console.log(pokemon)})
+            let cardPokemon = `
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${pokemon.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${pokemon.name}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    
+                </div>`
+            containerPokemon.innerHTML += cardPokemon
+    })
     });
