3617b8f3aeab39fa8fb2327ff78b44af33913126
Emilie <emilie.chnvt@gmail.com>
2024-11-21 18:52:07 +0100

    remove stats in ${pokemon.stats.attack}

--- js/main.js
+++ js/main.js
@@ -6,19 +6,12 @@ let url = 'https://pokebuildapi.fr/api/v1/pokemon/limit/20'; //PAS TOUS AFFICHÉ
 
 fetch(url)
  .then(response => response.json())
-    .then(data => displayPokemonsList(data))
-        containerPokemon.addEventListener('click', (e) => {
-
-            if(e.target.classList.contains('seeMore')) {
-                const pokemonId = e.target.getAttribute('data-id'); // ajouté sur le bouton
-                const selectedPokemon = data.filter((pokemon) => pokemon.id == pokemonId);
-                console.log(selectedPokemon);
-                if(selectedPokemon) displayPokemon(selectedPokemon);
+    .then(data => {
+        displayPokemonsList(data);
+        eventListener(data)
+    });
 
 
-
-            }
-        })
 
 
 function displayPokemonsList(data) {
@@ -37,21 +30,36 @@ function displayPokemonsList(data) {
                 </div>`
     containerPokemon.innerHTML += cardsPokemon
 })}
-function displayPokemon() {
+function displayPokemon(pokemon) {
     let cardPokemonsDetails = `
                         <div class="card border-warning poke w-50 h-50">
-                                <img src="${pokemon.image}" class="card-img-top w-100" alt="...">
+                                <img src="${pokemon}" class="card-img-top w-100" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title text-warning  fs-3">${pokemon.name}</h5>
-                                    <p class="card-text">Attack : ${pokemon.stats.attack}</p>
-                                    <p class="card-text">Defense : ${pokemon.stats.defense}</p>
-                                    <p class="card-text">Attack spéciale : ${pokemon.stats.special_attack}</p>
-                                    <p class="card-text">Defense spéciale: ${pokemon.stats.special_defense}</p>
-                                    <p class="card-text">Vitesse: ${pokemon.stats.speed}</p>
+                                    <p class="card-text">Attack : ${pokemon.attack}</p>
+                                    <p class="card-text">Defense : ${pokemon.defense}</p>
+                                    <p class="card-text">Attack spéciale : ${pokemon.special_attack}</p>
+                                    <p class="card-text">Defense spéciale: ${pokemon.special_defense}</p>
+                                    <p class="card-text">Vitesse: ${pokemon.speed}</p>
                                     <a href="#" class="btn btn-warning seeMore">Go Back</a> 
                                 </div>
                         
                         </div>`
 
-    containerPokemon.innerHTML = cardPokemonsDetails;
+    containerPokemon.innerHTML= cardPokemonsDetails;
+}
+function eventListener(data) {
+    containerPokemon.addEventListener('click', (e) => {
+
+        if(e.target.classList.contains('seeMore')) {
+            const pokemonId = e.target.getAttribute('data-id'); // ajouté sur le bouton
+            const selectedPokemon = data.filter((pokemon) => pokemon.id == pokemonId);
+            console.log(selectedPokemon);
+            if (selectedPokemon) displayPokemon(selectedPokemon);
+
+        }
+
+
+    })
+
 }
