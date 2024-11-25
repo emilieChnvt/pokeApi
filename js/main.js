326609882d3ad997af8118c4f0500f96a4ecffb4
Emilie <emilie.chnvt@gmail.com>
2024-11-25 21:22:06 +0100

    buttonAbilitiesClicked , displayPokemonsWithAbilities function added

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ fetch(apiPoke)
         displayAllPokemons(pokemonListArray);
 
 
+
     });
 
 
@@ -45,13 +46,52 @@ function displayButtonAbilities() {
     fetch('https://pokeapi.co/api/v2/ability/')
         .then(response => response.json())
         .then(data => {
+
             const listOfPokemonsAbilities = data.results;
             listOfPokemonsAbilities.forEach((ability) => {
                 let buttonAbility = document.createElement("button");
                 buttonAbility.textContent = ability.name;
                 buttonAbility.classList.add("w-10");
                 containerButton.appendChild(buttonAbility);
+
+                buttonAbility.addEventListener('click',()=>buttonAbilitiesClicked(ability.url));
             })
 
+
         })
+}
+
+function buttonAbilitiesClicked(abilityUrl) {
+
+        fetch(abilityUrl)//recupère les ability une par une
+            .then(response => response.json())
+            .then(data => {
+
+                let pokemonsWithAbilities = data.pokemon.map(ability => ability.pokemon) // pour trouver pokemon avec ability du boutton
+                console.log(pokemonsWithAbilities);
+                displayPokemonsWithAbilities(pokemonsWithAbilities);
+            })
+}
+function displayPokemonsWithAbilities(pokemons){
+    pokemons.forEach((pokemon) => {
+        fetch(pokemon.url)
+            .then(response => response.json())
+            .then(data => {
+
+                let pokemonCard = `
+                            <div class="card" style="width: 18rem;">
+                                <img src="${data.sprites.front_default}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                        <h5 class="card-title">${data.name}</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>`
+                containerPokemon.innerHTML += pokemonCard;
+
+            })
+
+
+    })
+
 }
