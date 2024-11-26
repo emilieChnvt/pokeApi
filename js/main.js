a2f4d0d5af1502f3db2d9074748d72e6f03276af
Emilie <emilie.chnvt@gmail.com>
2024-11-26 06:44:41 +0100

    styles added

--- js/main.js
+++ js/main.js
@@ -26,12 +26,12 @@ function displayAllPokemons(pokemonListArray) {
             .then(data => {
 
                 let pokemonCard = `
-                            <div class="card" style="width: 18rem;">
+                            <div class="card border border-warning" style="width: 18rem;">
                                 <img src="${data.sprites.front_default}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                         <h5 class="card-title">${data.name}</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        <p class="card-text">${data.effect}</p>
+                                        <a href="#" class="btn btn-warning">See More</a>
                                 </div>
                             </div>`
                 containerPokemon.innerHTML += pokemonCard;
